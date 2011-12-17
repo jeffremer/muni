2bb4bb9d27122751fa2406f74bb83f554e21bea7
Gun.io Whitespace Robot <contact@gun.io>
2011-12-17 03:00:34 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- spec/stop_spec.rb
+++ spec/stop_spec.rb
@@ -2,13 +2,13 @@ require 'muni/base'
 require 'muni/route'
 
 describe "Muni::Stop" do
-  it "#times fetches predictions" do   
+  it "#times fetches predictions" do
     route = Muni::Route.find(21)
     direction = route.directions.first
     stop = direction.stops.first
     times = stop.predictions
     times.each do |time|
       p time.pretty_time
-    end        
+    end
   end
 end
