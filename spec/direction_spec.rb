1cf23ad52c6e2c81d6d3cd8dc333e30559a945f3
Grégory Marcilhacy <g.marcilhacy@gmail.com>
2012-11-21 12:07:17 -0800

    minor styling tweaks in direction spec

--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -3,30 +3,24 @@ require 'spec_helper'
 describe Muni::Direction do
   use_vcr_cassette
 
-  before do
-    @route = Muni::Route.find(21)
-  end
+  let(:route) { Muni::Route.find(21) }
 
   context "#stop_at" do
     context "given  a stop tag" do
-      before do
-        @stop = @route.inbound.stop_at("36497")
-      end
-
       it "should return the corresponding stop" do
-        @stop.should be_present
-        @stop.title.should == "Steuart St & Mission St"
+        stop = route.inbound.stop_at("36497")
+
+        stop.should be_present
+        stop.title.should == "Steuart St & Mission St"
       end
     end
 
     context "given a stop title" do
-      before do
-        @stop  = @route.inbound.stop_at("Hayes St & Shrader St")
-      end
-
       it "should return corresponding stop" do
-        @stop.should be_present
-        @stop.title.should == "Hayes St & Shrader St"
+        stop  = route.inbound.stop_at("Hayes St & Shrader St")
+
+        stop.should be_present
+        stop.title.should == "Hayes St & Shrader St"
       end
     end
   end
