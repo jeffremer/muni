a34322792b2908fcdebfbcf5aea628820d64387b
Jeff Remer <jeff@strava.com>
2012-11-21 10:18:58 -0800

    Closes #2 by returning early if no predictions are available
    Also:
    
    * Updates gitignore
    * dates test dependencies to add vcr
    * Fixes indentation in Muni::Base
    * Fixes spacing in blocks
    * Adds VCR and fleshes out stop and route specs
    * Version bump to 0.0.3
    
    Thanks to @gregorym for bringing up #2.

--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -1,12 +1,15 @@
-require 'muni/base'
-require 'muni/route'
+require 'spec_helper'
 
-describe "Muni::Route" do
+describe Muni::Route do
+  use_vcr_cassette
+
   it "fetches all available routes" do
-    Muni::Route.find(:all)
+    routes = Muni::Route.find(:all)
+    routes.should_not be_empty
+    routes.first.tag.should == "F"
   end
 
   it "fetches routes by tag" do
-    Muni::Route.find(21)
+    Muni::Route.find(21).tag.should == "21"
   end
 end
--- spec/stop_spec.rb
+++ spec/stop_spec.rb
@@ -1,14 +1,19 @@
-require 'muni/base'
-require 'muni/route'
+require 'spec_helper'
 
-describe "Muni::Stop" do
-  it "#times fetches predictions" do
-    route = Muni::Route.find(21)
-    direction = route.directions.first
-    stop = direction.stops.first
-    times = stop.predictions
-    times.each do |time|
-      p time.pretty_time
+describe Muni::Stop do
+  use_vcr_cassette
+  context '#predictions' do
+    it 'fetches predictions' do
+      route = Muni::Route.find("38")
+      route.inbound.stops.first.predictions.should_not be_nil
+      prediction = route.inbound.stops.first.predictions.first
+      prediction.epochTime.to_i.should be > 0
     end
+
+    it 'should return an empty array if there are no predictions' do
+      Muni::Stop.stub(:fetch) { nil }
+      route = Muni::Route.find("38")
+      route.inbound.stops.first.predictions.should == []
+   end
   end
 end
