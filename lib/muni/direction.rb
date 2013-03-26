3381e308b07216006c8219649dd808293cec4bf1
Jeff Remer <jeff@threestarchina.com>
2013-03-26 12:51:07 -0700

    Properly finds stops with numeric titles
    This fixes an issue where stops with titles that started with numbers
    were being confused with stop tags.
    
    Fixes #9

--- lib/muni/direction.rb
+++ lib/muni/direction.rb
@@ -3,7 +3,14 @@ require "amatch"
 module Muni
   class Direction < Base
     def stop_at(place)
-      return stops.select{ |stop| stop.tag == place }.first if place =~ /[1-9][0-9]+/
+      if stop = stops.detect { |stop| stop.tag == place }
+        return stop
+      end
+
+      if stop = stops.detect { |stop| stop.title == place }
+        return stop
+      end
+
       pattern = Amatch::Sellers.new(place)
       stops.sort_by{ |stop|
         pattern.match(stop.title)
