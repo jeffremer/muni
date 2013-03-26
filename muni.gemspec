c8707e225a9f67d081c376dade985c063da08f1d
Jeff Remer <jeff@threestarchina.com>
2013-03-26 12:51:50 -0700

    Fixes VCR/WebMock version warnings

--- muni.gemspec
+++ muni.gemspec
@@ -23,6 +23,6 @@ Gem::Specification.new do |s|
   s.add_dependency "amatch"
   s.add_dependency "thor"   # For the executable
   s.add_development_dependency "rspec", "~> 2"
-  s.add_development_dependency "webmock"
+  s.add_development_dependency "webmock", "1.9.0"
   s.add_development_dependency "vcr"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,9 @@ require 'muni'
 require 'vcr'
 
 VCR.configure do |c|
+  c.configure_rspec_metadata!
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.default_cassette_options = { :record => :new_episodes }
 end
 
-RSpec.configure do |config|
-  config.extend VCR::RSpec::Macros
-  config.expect_with(:rspec){ |c| c.syntax = [:should, :expect] }
-end
