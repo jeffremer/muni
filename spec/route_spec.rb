b1c3101ed1a3bead56549c0e4cd5de1d7dd6773c
James Hwang <jhwang@newrelic.com>
2013-07-07 00:29:51 -0700

    Add test to cover lat and lon accuracy

--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -10,4 +10,10 @@ describe Muni::Route, vcr: true do
   it "fetches routes by tag" do
     Muni::Route.find(21).tag.should == "21"
   end
+
+  it "should have the correct lon and lat" do
+    r21 = Muni::Route.find(21)
+    r21.inbound.stops.first.lon.should == "-122.46384"
+    r21.inbound.stops.first.lat.should == "37.7735299"
+  end
 end
